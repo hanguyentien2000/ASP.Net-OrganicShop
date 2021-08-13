2c04fd2e53f17a2f7ee1b128e3118c3e84313cef
unknown <allfallsdown20@gmail.com>
2021-08-13 22:37:25 +0700

    load product

---
+++ LeafShop/Controllers/SanPhamController.cs
@@ -0,0 +1,54 @@
+﻿using LeafShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeafShop.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        public ActionResult Index()
+        {
+            var dbContext = new LeafShopDb();
+            List<SanPham> listall = dbContext.SanPhams.ToList();
+            return View();
+        }
+        LeafShopDb db = new LeafShopDb();
+        //public ActionResult Products(string SearchString, string currentFilter, int? page)
+        //{
+        //    if (SearchString != null)
+        //    {
+        //        page = 1;
+        //    }
+        //    else
+        //    {
+        //        SearchString = currentFilter;
+        //    }
+        //    ViewBag.CurrentFilter = SearchString;
+        //    var danhmucs = db.DanhMucs.Select(d => d);
+        //    if (!String.IsNullOrEmpty(SearchString))
+        //    {
+        //        danhmucs = danhmucs.Where(p => p.TenDanhMuc.Contains(SearchString));
+        //    }
+        //    int pageSize = 10;
+
+        //    int pageNumber = (page ?? 1);
+        //    return View(danhmucs.ToPagedList(pageNumber, pageSize));
+        //}
+        public ActionResult Category(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            var product = db.SanPhams.Where(s => s.MaDanhMuc == id);
+            ViewBag.listProduct = product;
+            return View();
+        }
+    }
+}
