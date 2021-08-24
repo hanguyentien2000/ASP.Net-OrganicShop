1d1c1cd92036077825d7ab1c4d564f0c55cd82cb
hanguyentien2000 <hanguyentien2000@gmail.com>
2021-08-25 00:39:11 +0700

    fix bug dathang

--- LeafShop/Areas/Administrator/Controllers/DatHangController.cs
+++ LeafShop/Areas/Administrator/Controllers/DatHangController.cs
@@ -65,7 +65,7 @@ namespace LeafShop.Areas.Administrator.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "MaDatHang,MaKhachHang,MaNhanVien,TongTien,NgayKhoiTao,NgayGiaoHang,GhiChu,DiaChi,TrangThai")] DatHang datHang)
+        public ActionResult Create(DatHang datHang)
         {
             try
             {
@@ -77,9 +77,20 @@ namespace LeafShop.Areas.Administrator.Controllers
                 {
                     ViewBag.Error = "Mã đặt hàng này đã tồn tại";
                 }
-                if (ModelState.IsValid)
+                else if (existData == null)
                 {
-                    db.DatHangs.Add(datHang);
+                    var data = new DatHang
+                    {
+                        MaKhachHang = datHang.MaKhachHang,
+                        MaNhanVien = datHang.MaNhanVien,
+                        DiaChi = datHang.DiaChi,
+                        GhiChu = datHang.GhiChu,
+                        NgayKhoiTao = DateTime.Now,
+                        NgayGiaoHang = null,
+                        TrangThai = datHang.TrangThai,
+                        TongTien = datHang.TongTien
+                    };
+                    db.DatHangs.Add(data);
                     db.SaveChanges();
                 }
                 return RedirectToAction("Index");
@@ -109,21 +120,33 @@ namespace LeafShop.Areas.Administrator.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "MaDatHang,MaKhachHang,MaNhanVien,TongTien,NgayKhoiTao,NgayGiaoHang,GhiChu,DiaChi,TrangThai")] DatHang datHang)
+        public ActionResult Edit(DatHang datHang)
         {
-            if (ModelState.IsValid)
+            DatHang dathangs = db.DatHangs.Where(s => s.MaDatHang == datHang.MaDatHang).FirstOrDefault();
+            dathangs.MaKhachHang = datHang.MaKhachHang;
+            dathangs.MaNhanVien = datHang.MaNhanVien;
+            dathangs.NgayKhoiTao = datHang.NgayKhoiTao;
+            dathangs.TongTien = datHang.TongTien;
+            dathangs.DiaChi = datHang.DiaChi;
+            dathangs.GhiChu = datHang.GhiChu;
+
+            if(dathangs.TrangThai == true)
             {
-                db.Entry(datHang).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                dathangs.TrangThai = datHang.TrangThai;
+                dathangs.NgayGiaoHang = DateTime.Now;
             }
-            ViewBag.MaKhachHang = new SelectList(db.KhachHangs, "MaKhachHang", "TenKhachHang", datHang.MaKhachHang);
-            ViewBag.MaNhanVien = new SelectList(db.NhanViens, "MaNhanVien", "TenNhanVien", datHang.MaNhanVien);
-            return View(datHang);
+            else if(dathangs.TrangThai == false)
+            {
+                dathangs.TrangThai = datHang.TrangThai;
+                dathangs.NgayGiaoHang = null;
+            } 
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        // GET: Administrator/DatHang/Delete/5
-        public ActionResult Delete(int id)
+            // GET: Administrator/DatHang/Delete/5
+            public ActionResult Delete(int id)
         {
             DatHang datHang = db.DatHangs.Find(id);
             if (datHang == null)
